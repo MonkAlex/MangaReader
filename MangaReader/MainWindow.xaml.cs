471fb764dea4dd2791d32e31fa2ebd0d941ed099
MonkAlex <3amepob@gmail.com>
2014-03-10 23:52:38 +0400

    Поддержка абсолютных и относительных путей при упаковке.

--- MangaReader/MainWindow.xaml.cs
+++ MangaReader/MainWindow.xaml.cs
@@ -14,9 +14,15 @@ namespace MangaReader
 
         private void button1_Click(object sender, RoutedEventArgs e)
         {
-            var manga = new Manga("http://readmanga.me/akb49___the_rules_against_love");
-            manga.Download(manga.Name, "Том_", "Глава_");
-          //Comperssion.ComperssVolumes(manga.Name);
+            var manga = new Manga("http://readmanga.me/toradora");
+            //manga.Download(manga.Name, "Том_", "Глава_");
+            Comperssion.ComperssChapters(@"E:\Docs\Visual Studio 2010\Projects\MangaReader\MangaReader\bin\Debug\ToraDora");
+
+          /*var manga2 = new Manga("http://readmanga.me/saki");
+            manga2.Download(manga2.Name, "Том_", "Глава_");
+            Comperssion.ComperssVolumes(manga2.Name);*/
+            
+            MessageBox.Show("ok");
         }
     }
 }
--- MangaReader/Services/Comperssion.cs
+++ MangaReader/Services/Comperssion.cs
@@ -1,5 +1,7 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.IO.Compression;
+using System.Linq;
 using System.Text;
 
 namespace MangaReader
@@ -17,10 +19,11 @@ namespace MangaReader
                 var volumes = Directory.GetDirectories(message);
                 foreach (var volume in volumes)
                 {
-                    var chapters = Directory.GetDirectories(volume);
+                    Directory.SetCurrentDirectory(volume);
+                    var chapters = Directory.GetDirectories(".\\");
                     foreach (var chapter in chapters)
                     {
-                        var acr = chapter + ".zip";
+                        var acr = string.Concat(GetFolderName(message), "_", GetFolderName(volume), "_", GetFolderName(chapter), ".zip");
                         if (File.Exists(acr))
                             File.Delete(acr);
                         ZipFile.CreateFromDirectory(chapter, acr, CompressionLevel.NoCompression, false, Encoding.UTF8);
@@ -38,16 +41,29 @@ namespace MangaReader
         {
             if (Directory.Exists(message))
             {
-                var volumes = Directory.GetDirectories(message);
+                Directory.SetCurrentDirectory(message);
+                var volumes = Directory.GetDirectories(".\\");
                 foreach (var volume in volumes)
                 {
-                    var acr = volume + ".zip";
+                    var acr = string.Concat(GetFolderName(message), "_", GetFolderName(volume), ".zip");
                     if (File.Exists(acr))
                         File.Delete(acr);
                     ZipFile.CreateFromDirectory(volume, acr, CompressionLevel.NoCompression, false, Encoding.UTF8);
                     Directory.Delete(volume, true);
                 }
             }
+        }
+
+        /// <summary>
+        /// Вернуть название папки.
+        /// </summary>
+        /// <param name="path">Путь к папке. Абсолютный или относительный.</param>
+        /// <returns>Название папки. '.\Folder\' -> 'Folder'</returns>
+        private static string GetFolderName(string path)
+        {
+            return path.Split(new char[] { Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar },
+                            StringSplitOptions.RemoveEmptyEntries)
+                       .Last();
         }
     }
 }

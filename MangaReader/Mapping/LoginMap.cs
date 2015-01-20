0d273ab151efcca7b8d47113b0533d5171af71b7
MonkAlex <3amepob@gmail.com>
2015-01-20 08:34:02 +0400

    Забыл добавить мапинг логинов.

---
+++ MangaReader/Mapping/LoginMap.cs
@@ -0,0 +1,15 @@
+﻿using FluentNHibernate.Mapping;
+using MangaReader.Account;
+
+namespace MangaReader.Mapping
+{
+  public class LoginMap : ClassMap<Login>
+  {
+    public LoginMap()
+    {
+      Id(x => x.Id);
+      Map(x => x.Name).Not.LazyLoad();
+      Map(x => x.Password).Not.LazyLoad();
+    }
+  }
+}

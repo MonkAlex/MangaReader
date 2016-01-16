28091ff01674217a4ee0ed75bde7710094c54e06
MonkAlex <3amepob@gmail.com>
2016-01-16 09:56:09 +0400

    Опять файл отцепился.

---
+++ MangaReader/UI/Converting.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+
+namespace MangaReader.Services
+{
+  /// <summary>
+  /// Логика взаимодействия для Converting.xaml
+  /// </summary>
+  public partial class Converting : Window
+  {
+    public Converting()
+    {
+      InitializeComponent();
+    }
+
+    private void Converting_OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+    {
+      var oldValue = e.OldValue as IProcess;
+      if (oldValue != null)
+        oldValue.StateChanged -= OnStateChanged;
+
+      var value = e.NewValue as IProcess;
+      if (value != null)
+        value.StateChanged += OnStateChanged;
+    }
+
+    private void OnStateChanged(object sender, ConvertState convertState)
+    {
+      if (convertState != ConvertState.Completed)
+        return;
+
+      Close();
+    }
+  }
+}

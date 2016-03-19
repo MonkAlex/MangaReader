7f3f65f5e8e43e8b8c43d42b2a4838f9397e7aab
MonkAlex <3amepob@gmail.com>
2016-03-19 05:11:24 +0400

    Тесты на историю изменений.

---
+++ Tests/Tests.API/VersionInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.API
+{
+  [TestClass]
+  public class VersionInfo
+  {
+    [TestMethod]
+    public void VersionHistoryIsExists()
+    {
+      var text = MangaReader.Core.Update.VersionHistory.GetHistory();
+      Assert.IsTrue(!string.IsNullOrWhiteSpace(text));
+    }
+
+    [TestMethod]
+    public void VersionHistoryCorrect()
+    {
+      var version = MangaReader.Core.Update.VersionHistory.GetVersion();
+      Assert.AreEqual(MangaReader.Core.Services.Config.AppConfig.Version, version);
+    }
+  }
+}

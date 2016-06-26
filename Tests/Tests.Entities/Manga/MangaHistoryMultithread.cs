1ef1c253e9e4e7f30f6b7e960abae44b6ea31510
MonkAlex <3amepob@gmail.com>
2016-06-26 15:14:51 +0400

    тест на многопоточное добавление истории. #66

---
+++ Tests/Tests.Entities/Manga/MangaHistoryMultithread.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using MangaReader.Core.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.Entities.Manga
+{
+  [TestClass]
+  public class MangaHistoryMultithread
+  {
+    [TestMethod]
+    public void AddHistoryMultithread()
+    {
+      var manga = Builder.CreateReadmanga();
+      Parallel.For(0, 100, i =>
+      {
+        manga.AddHistory(new Uri(string.Format("http://readmanga.me/btoom/vol1/{0}?mature=1", i)));
+      });
+      Assert.AreEqual(10000, manga.Histories.Count);
+    }
+  }
+}

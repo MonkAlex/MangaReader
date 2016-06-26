e2a57e22300663066f3ecf32e4a5a4e5212b7f7f
MonkAlex <3amepob@gmail.com>
2016-06-26 17:04:57 +0400

    Поправил тест, история теперь пересекается.

--- Tests/Tests.Entities/Manga/MangaHistoryMultithread.cs
+++ Tests/Tests.Entities/Manga/MangaHistoryMultithread.cs
@@ -12,12 +12,13 @@ namespace Tests.Entities.Manga
     public void AddHistoryMultithread()
     {
       var manga = Builder.CreateReadmanga();
-      var x = 1000;
+      var x = 100000;
+      var y = 100;
       Parallel.For(0, x, i =>
       {
-        manga.AddHistory(new Uri(string.Format("http://readmanga.me/btoom/vol1/{0}?mature=1", i)));
+        manga.AddHistory(new Uri(string.Format("http://readmanga.me/btoom/vol1/{0}?mature=1", i % y)));
       });
-      Assert.AreEqual(x, manga.Histories.Count());
+      Assert.AreEqual(y, manga.Histories.Count());
     }
   }
 }

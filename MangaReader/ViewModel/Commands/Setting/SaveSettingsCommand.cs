ea4d60954ad8fcba28bef33c4c37b85af473e501
MonkAlex <3amepob@gmail.com>
2016-06-26 14:54:45 +0400

    Запрос на ребут при смене скина. Fixed #67

--- MangaReader/ViewModel/Commands/Setting/SaveSettingsCommand.cs
+++ MangaReader/ViewModel/Commands/Setting/SaveSettingsCommand.cs
@@ -1,4 +1,7 @@
-﻿using MangaReader.Core.Services.Config;
+﻿using System.Diagnostics;
+using System.Windows;
+using MangaReader.Core.Services.Config;
+using MangaReader.Services;
 using MangaReader.UI.Services;
 using MangaReader.ViewModel.Commands.Primitives;
 using MangaReader.ViewModel.Setting;
@@ -13,10 +16,23 @@ namespace MangaReader.ViewModel.Commands.Setting
     {
       base.Execute(parameter);
 
+      var skinChanged = ConfigStorage.Instance.ViewConfig.SkinGuid != settingModel.AppSetting.Skin.Guid;
+
       foreach (var mangaSetting in settingModel.Views)
         mangaSetting.Save();
 
       ConfigStorage.Instance.Save();
+
+      if (skinChanged)
+      {
+        var restart = Dialogs.ShowYesNoDialog("Перезапуск", "Перезапустить программу?", "Необходим перезапуск для применения новых настроек.");
+        if (restart)
+        {
+          // TODO: потенциальная гонка условий, может не запуститься.
+          new ExitCommand().Execute(parameter);
+          Process.Start(Application.ResourceAssembly.Location);
+        }
+      }
 
       var window = ViewService.Instance.TryGet<System.Windows.Window>(settingModel);
       if (window != null)

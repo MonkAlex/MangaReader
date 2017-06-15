eadc8a705d479b488130d9d989039070ff5796dd
MonkAlex <3amepob@gmail.com>
2017-06-15 19:02:10 +0400

    Вернул билдющуюся заглушку для Авалонии.

---
+++ MangaReader.Avalonia/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace MangaReader.Avalonia
+{
+  public class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      this.InitializeComponent();
+      App.AttachDevTools(this);
+    }
+
+    private void InitializeComponent()
+    {
+      AvaloniaXamlLoader.Load(this);
+    }
+  }
+}

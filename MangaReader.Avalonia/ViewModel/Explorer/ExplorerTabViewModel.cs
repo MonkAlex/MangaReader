464920ce639efb32b04c7d7e1fbbcd6467be0a6b
MonkAlex <3amepob@gmail.com>
2017-07-07 00:33:14 +0400

    Основная идея того, как будут модельки окна выглядеть.

---
+++ MangaReader.Avalonia/ViewModel/Explorer/ExplorerTabViewModel.cs
@@ -0,0 +1,27 @@
+﻿namespace MangaReader.Avalonia.ViewModel.Explorer
+{
+  public class ExplorerTabViewModel<T> : ViewModelBase where T : ViewModelBase
+  {
+    private string name;
+    private int priority;
+    private T content;
+
+    public string Name
+    {
+      get { return name; }
+      set { RaiseAndSetIfChanged(ref name, value); }
+    }
+
+    public int Priority
+    {
+      get { return priority; }
+      set { RaiseAndSetIfChanged(ref priority, value); }
+    }
+
+    public T Content
+    {
+      get { return content; }
+      set { RaiseAndSetIfChanged(ref content, value); }
+    }
+  }
+}
---
+++ MangaReader.Avalonia/ViewModel/ExplorerViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.ObjectModel;
+using MangaReader.Avalonia.ViewModel.Explorer;
+
+namespace MangaReader.Avalonia.ViewModel
+{
+  public class ExplorerViewModel : ViewModelBase
+  {
+    public ObservableCollection<ExplorerTabViewModel<ViewModelBase>> Tabs { get; }
+
+    public ExplorerViewModel()
+    {
+      this.Tabs = new ObservableCollection<ExplorerTabViewModel<ViewModelBase>>();
+    }
+  }
+}

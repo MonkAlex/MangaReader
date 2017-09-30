fa96004718d35e1a8cd7b91be3dba752deaa45be
MonkAlex <3amepob@gmail.com>
2017-09-30 16:33:35 +0400

    #102 test on master branch, fixed now in avalonia.

--- Tests/Tests.Entities/Manga/MangaHistoryMultithread.cs
+++ Tests/Tests.Entities/Manga/MangaHistoryMultithread.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Grouple;
+using MangaReader.Core.Exception;
+using MangaReader.Core.NHibernate;
+using MangaReader.Core.Services;
 using NUnit.Framework;
 
 namespace Tests.Entities.Manga
@@ -18,25 +23,33 @@ namespace Tests.Entities.Manga
       Assert.AreEqual(y, manga.Histories.Count());
     }
 
+    /// <summary>
+    /// #102 - после исключения в beforesave история одной манги мешает создавать историю другой манги.
+    /// </summary>
     [Test]
     public void AddHistoryMultithreadAndSave()
     {
       var manga = Builder.CreateReadmanga();
       var name = manga.Name;
       manga.Save();
+      Directory.CreateDirectory(manga.GetAbsoulteFolderPath());
+      var manga2 = Builder.CreateReadmanga();
 
-      var exception = Assert.Catch(() =>
+      var exception = Assert.Catch<TargetInvocationException>(() =>
       {
-        manga = Builder.CreateReadmanga();
-        manga.Name = name;
-        AddRandomHistory(manga);
-        manga.Save();
+        var manga3 = Builder.CreateReadmanga();
+        Directory.CreateDirectory(manga3.GetAbsoulteFolderPath());
+        manga3.Name = name;
+        AddRandomHistory(manga3);
+        Assert.AreEqual(y, manga3.Histories.Count());
+        manga3.Save();
       });
-      Assert.NotNull(exception);
+      Assert.IsAssignableFrom<SaveValidationException>(exception.InnerException);
 
-      manga = Builder.CreateReadmanga();
-      AddRandomHistory(manga);
-      manga.Save();
+      AddRandomHistory(manga2);
+      manga2.Save();
+      Assert.AreEqual(y, manga2.Histories.Count());
+      Assert.AreEqual(y, Repository.Get<MangaReader.Core.Manga.MangaHistory>().Count());
     }
 
     private static void AddRandomHistory(Readmanga manga)

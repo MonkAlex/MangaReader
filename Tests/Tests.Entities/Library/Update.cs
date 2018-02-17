cdb3de434ecb053e63e2a4c7f2848e0785084321
MonkAlex <3amepob@gmail.com>
2018-02-17 16:45:29 +0400

    #110 Fix autoupdate, when library sort by name. Add tests for cases.

---
+++ Tests/Tests.Entities/Library/Update.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ComponentModel;
+using MangaReader.Core.Manga;
+using MangaReader.Core.NHibernate;
+using MangaReader.Core.Services;
+using MangaReader.Core.Services.Config;
+using NUnit.Framework;
+
+namespace Tests.Entities.Library
+{
+  [TestFixture]
+  [Parallelizable(ParallelScope.None)]
+  public class Update : TestClass
+  {
+    [Test]
+    public void TestUpdate()
+    {
+      var library = new LibraryViewModel();
+      using (var context = Repository.GetEntityContext())
+      foreach (var forDelete in context.Get<IManga>())
+        forDelete.Delete();
+      var manga = Builder.CreateReadmanga();
+      TestUpdateFromConfig(library, ListSortDirection.Ascending, nameof(IManga.DownloadedAt));
+      TestUpdateFromConfig(library, ListSortDirection.Ascending, nameof(IManga.Created));
+      TestUpdateFromConfig(library, ListSortDirection.Ascending, nameof(IManga.Name));
+      manga.Delete();
+    }
+
+    private void TestUpdateFromConfig(LibraryViewModel library, ListSortDirection direction, string property)
+    {
+      ConfigStorage.Instance.ViewConfig.LibraryFilter.SortDescription = new SortDescription()
+        {Direction = direction, PropertyName = property};
+      var lastErrorMessage = string.Empty;
+
+      void OnLogReceived(LogEventStruct les)
+      {
+        if (les.Level == "Error")
+          lastErrorMessage = les.FormattedMessage;
+      }
+
+      Log.LogReceived += OnLogReceived;
+      library.Update();
+      Log.LogReceived -= OnLogReceived;
+      if (!string.IsNullOrWhiteSpace(lastErrorMessage))
+        Assert.Fail(lastErrorMessage);
+    }
+
+  }
+}
---
+++ Tests/Tests.Entities/Manga/MangaLocalName.cs
@@ -0,0 +1,43 @@
+﻿using MangaReader.Core.NHibernate;
+using NUnit.Framework;
+
+namespace Tests.Entities.Manga
+{
+  [TestFixture]
+  public class MangaLocalName : TestClass
+  {
+    [Test]
+    public void SetLocalName()
+    {
+      var mangaLocalName = "LocalName";
+      var manga = Builder.CreateReadmanga();
+      var serverName = manga.Name;
+      Assert.AreEqual(serverName, manga.Name);
+      Assert.AreEqual(serverName, manga.ServerName);
+      Assert.AreEqual(serverName, manga.LocalName);
+
+      manga.IsNameChanged = true;
+      manga.Name = mangaLocalName;
+      Assert.AreEqual(mangaLocalName, manga.Name);
+      Assert.AreEqual(mangaLocalName, manga.LocalName);
+      Assert.AreEqual(serverName, manga.ServerName);
+
+      manga.Save();
+      manga = Repository.GetStateless<Grouple.Readmanga>(manga.Id);
+      Assert.AreEqual(mangaLocalName, manga.Name);
+      Assert.AreEqual(mangaLocalName, manga.LocalName);
+      Assert.AreEqual(serverName, manga.ServerName);
+
+      manga.IsNameChanged = false;
+      Assert.AreEqual(serverName, manga.Name);
+      Assert.AreEqual(mangaLocalName, manga.LocalName);
+      Assert.AreEqual(serverName, manga.ServerName);
+
+      manga.Save();
+      manga = Repository.GetStateless<Grouple.Readmanga>(manga.Id);
+      Assert.AreEqual(serverName, manga.Name);
+      Assert.AreEqual(mangaLocalName, manga.LocalName);
+      Assert.AreEqual(serverName, manga.ServerName);
+    }
+  }
+}

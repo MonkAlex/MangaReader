575ed455c9da90bc850ad2b264a4beeb23fa2d92
MonkAlex <3amepob@gmail.com>
2018-04-30 22:37:38 +0400

    Test up.

--- Tests/Tests.Convertation/1.12/Convert.cs
+++ Tests/Tests.Convertation/1.12/Convert.cs
@@ -25,7 +25,7 @@ namespace Tests.Convertation._1._12
       using (var context = Repository.GetEntityContext())
       {
         var loadedMangas = context.Get<IManga>().ToList();
-        Assert.AreEqual(33, loadedMangas.Count);
+        Assert.AreEqual(32, loadedMangas.Count);
 
         var loadedHistoryRecord = context.Get<MangaHistory>().ToList();
 
@@ -41,8 +41,7 @@ namespace Tests.Convertation._1._12
         Assert.AreEqual(36, loadedHistoryRecord.Count(h => h.Url.Contains("area_d")));
         Assert.AreEqual(226, loadedHistoryRecord.Count(h => h.Url.Contains("b_type_h_style")));
         Assert.AreEqual(7, loadedHistoryRecord.Count(h => h.Url.Contains("baka_and_boing")));
-        Assert.AreEqual(20, loadedHistoryRecord.Count(h => h.Url.Contains("the_idiot__the_test__and_summoned_creatures")));
-        Assert.AreEqual(18, loadedHistoryRecord.Count(h => h.Url.Contains("berserk")));
+        Assert.AreEqual(20, loadedHistoryRecord.Count(h => h.Url.Contains("baka_and_test__summon_the_beasts")));
         Assert.AreEqual(52, loadedHistoryRecord.Count(h => h.Url.Contains("the_god_of_poverty_is")));
         Assert.AreEqual(50, loadedHistoryRecord.Count(h => h.Url.Contains("i_love_my_little_sister")));
         Assert.AreEqual(6, loadedHistoryRecord.Count(h => h.Url.Contains("come_and_wild_vanilla")));
@@ -70,7 +69,7 @@ namespace Tests.Convertation._1._12
         Assert.AreEqual(18, loadedHistoryRecord.Count(h => h.Url.Contains("this_older_woman_is_a_fiction")));
 
         Assert.AreEqual(loadedMangas.Sum(m => m.Histories.Count()), loadedHistoryRecord.Count);
-        Assert.AreEqual(2772, loadedHistoryRecord.Count);
+        Assert.AreEqual(2754, loadedHistoryRecord.Count);
         // 1.12 history create records now.
         Assert.Less(startDate, loadedMangas.Min(m => m.Created));
       }

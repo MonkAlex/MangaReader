340bdc5b68712ff2c5fa133cce3c721597d09623
MonkAlex <3amepob@gmail.com>
2018-05-08 20:39:25 +0400

    Test on library vm events and public properties.

--- Tests/Tests.Entities/Library/Update.cs
+++ Tests/Tests.Entities/Library/Update.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
 using MangaReader.Core.Manga;
 using MangaReader.Core.NHibernate;
 using MangaReader.Core.Services;
@@ -10,10 +13,10 @@ using SortDescription = MangaReader.Core.Services.Config.SortDescription;
 namespace Tests.Entities.Library
 {
   [TestFixture]
-  [Parallelizable(ParallelScope.None)]
   public class Update : TestClass
   {
     [Test]
+    [Parallelizable(ParallelScope.None)]
     public void TestUpdate()
     {
       var library = new LibraryViewModel();
@@ -44,6 +47,49 @@ namespace Tests.Entities.Library
       Log.LogReceived -= OnLogReceived;
       if (!string.IsNullOrWhiteSpace(lastErrorMessage))
         Assert.Fail(lastErrorMessage);
+    }
+
+    [Test]
+    public async Task UpdateMangaWithPause()
+    {
+      var events = new List<LibraryViewModelArgs>();
+      void LibraryOnLibraryChanged(object sender, LibraryViewModelArgs e)
+      {
+        events.Add(e);
+      }
+
+      var manga = Builder.CreateReadmanga();
+      var library = new LibraryViewModel();
+      library.LibraryChanged += LibraryOnLibraryChanged;
+
+      var inProcess = false;
+      Assert.AreEqual(!inProcess, library.IsAvaible);
+      Assert.AreEqual(false, library.IsPaused);
+      Assert.AreEqual(inProcess, library.InProcess);
+
+      var task = library.ThreadAction(() => library.Update(new List<int>(){manga.Id}, mangas => mangas.OrderBy(m => m.DownloadedAt)));
+
+      Assert.AreEqual(task.IsCompleted, library.IsAvaible);
+      Assert.AreEqual(false, library.IsPaused);
+      Assert.AreEqual(!task.IsCompleted, library.InProcess);
+
+      library.IsPaused = true;
+
+      Assert.AreEqual(task.IsCompleted, library.IsAvaible);
+      Assert.AreEqual(true, library.IsPaused);
+      Assert.AreEqual(!task.IsCompleted, library.InProcess);
+
+      library.IsPaused = false;
+
+      await task;
+
+      library.Remove(manga);
+      library.LibraryChanged -= LibraryOnLibraryChanged;
+      Assert.AreEqual(0, manga.Id);
+
+      Assert.Contains(new LibraryViewModelArgs(null, null, MangaOperation.None, LibraryOperation.UpdateStarted), events);
+      Assert.Contains(new LibraryViewModelArgs(null, null, MangaOperation.None, LibraryOperation.UpdateCompleted), events);
+      Assert.Contains(new LibraryViewModelArgs(null, manga, MangaOperation.Deleted, LibraryOperation.UpdateMangaChanged), events);
     }
 
   }

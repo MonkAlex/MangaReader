25d21c764c66cb6f9b4ef4d3c4b904a3e6600652
MonkAlex <3amepob@gmail.com>
2018-12-22 15:06:36 +0400

    Mintmanga download test.

--- Tests/Tests.Entities/Download/Readmanga.cs
+++ Tests/Tests.Entities/Download/Readmanga.cs
@@ -40,6 +40,29 @@ namespace Tests.Entities.Download
       Assert.AreEqual(100, lastPercent);
     }
 
+    [Test]
+    public async Task DownloadMintmanga()
+    {
+      var rm = Mangas.CreateFromWeb(new Uri(@"http://mintmanga.com/haruka_na_receive"));
+      var sw = new Stopwatch();
+      sw.Start();
+      rm.PropertyChanged += RmOnDownloadChanged;
+      DirectoryHelpers.DeleteDirectory(rm.GetAbsoulteFolderPath());
+      await rm.Download();
+      sw.Stop();
+      Log.Add($"manga loaded {sw.Elapsed.TotalSeconds}, iscompleted = {rm.IsDownloaded}, lastpercent = {lastPercent}");
+      Assert.IsTrue(Directory.Exists(rm.GetAbsoulteFolderPath()));
+      var files = Directory.GetFiles(rm.GetAbsoulteFolderPath(), "*", SearchOption.AllDirectories);
+      Assert.AreEqual(11, files.Length);
+      var fileInfos = files.Select(f => new FileInfo(f)).ToList();
+      Assert.AreEqual(3474445, fileInfos.Sum(f => f.Length));
+
+      // Количество повторяющихся картинок на мангу - не больше одной на главу.
+      Assert.IsTrue(rm.Volumes.Sum(v => v.Container.Count()) >= fileInfos.GroupBy(f => f.Length).Max(g => g.Count()));
+      Assert.IsTrue(rm.IsDownloaded);
+      Assert.AreEqual(100, lastPercent);
+    }
+
     private void RmOnDownloadChanged(object sender, PropertyChangedEventArgs args)
     {
       if (args.PropertyName == nameof(IManga.Downloaded))

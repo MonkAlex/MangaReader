fc2fd932685859f3c593fe4e0a8f522a49b4ea78
MonkAlex <3amepob@gmail.com>
2019-03-22 22:42:03 +0400

    #136 Try to update Avalonia -- dialogs has troubles now, maybe time to rework dialogs.

--- MangaReader.Avalonia/Services/ViewConfigEx.cs
+++ MangaReader.Avalonia/Services/ViewConfigEx.cs
@@ -21,7 +21,7 @@ namespace MangaReader.Avalonia.Services
         main.WindowState = (AvaloniaWindowState)Enum.Parse(typeof(ConfigWindowState), config.WindowStates.WindowState.ToString(), true);
         if (config.WindowStates.CanShow)
         {
-          main.Position = new Point(config.WindowStates.Left, config.WindowStates.Top);
+          main.Position = PixelPoint.FromPoint(new Point(config.WindowStates.Left, config.WindowStates.Top), 1);
           main.Width = config.WindowStates.Width;
           main.Height = config.WindowStates.Height;
         }
